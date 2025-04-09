5f0cd1f0b48bc4995476def7618352ab4bc96d69
doif-pc <josh.is.ahern@gmail.com>
2025-04-09 13:59:32 -0600

    simple movement

--- Entities/test_bean/test_bean.gd
+++ Entities/test_bean/test_bean.gd
@@ -1,17 +1,29 @@
 extends RigidBody3D
 
 var angle_look: Vector2 = Vector2(0.0, 0.0)
+var move_speed: float = 10.0 #units/sec target
 
+var vel_target_2d: Vector2 = Vector2(0.0, 0.0) #x is right/left, y is front/back
 
 func _ready() -> void:
 	pass
 	
 func _process(delta: float) -> void:
-	pass
+	angle_look = Global.angle_look
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("game_jump"):
 		self.apply_central_impulse(Vector3(0.0, 5 * self.mass, 0.0))
+		
+	vel_target_2d = Global.get_horizontal_movement_from_keyboard()
+	vel_target_2d = vel_target_2d * move_speed
+	
+	var vel_target_3d = Vector3(vel_target_2d.x, self.linear_velocity.y, -vel_target_2d.y)
+	vel_target_3d = vel_target_3d.rotated(Vector3(0.0, 1.0, 0.0), -angle_look.x)
+	
+	var vel_error_3d = vel_target_3d - self.linear_velocity
+	Global.angle_walk = Vector2(self.linear_velocity.x, self.linear_velocity.z)
+	self.apply_central_impulse(5.0 * vel_error_3d * delta * self.mass)
 	
 func _unhandled_input(event: InputEvent) -> void:
 	pass
