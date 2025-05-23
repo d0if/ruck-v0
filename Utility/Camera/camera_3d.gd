99ffc433012553d6b16955e7eded5e314e50cc88
PCMeier <pcmeier128@gmail.com>
2025-05-23 13:43:12 -0600

    selecter

--- Utility/Camera/camera_3d.gd
+++ Utility/Camera/camera_3d.gd
@@ -1,50 +1,65 @@
 extends Camera3D
 
 @export var selectray: RayCast3D
-var firstobject: Node = null
-var current_mode: String = ""
-var interacted_this_hold := {}
+var current_mode: String = ""       # "select", "deselect", or ""
+var interacted_this_hold := {}      # Track objects already acted on during hold
+var hovered: Node = null
 
 func _process(_delta):
-	# Clear hold tracking when key is first pressed
+	if selectray.is_colliding():
+		var hit = selectray.get_collider()
+		if hit.is_in_group("Selectable"):
+			if hovered != hit:
+				if hovered and hovered.is_in_group("Hover"):
+					hovered.remove_from_group("Hover")
+				hit.add_to_group("Hover")
+				hovered = hit
+		else:
+			if hovered and hovered.is_in_group("Hover"):
+				hovered.remove_from_group("Hover")
+			hovered = null
+	else:
+		if hovered and hovered.is_in_group("Hover"):
+			hovered.remove_from_group("Hover")
+		hovered = null
+	# On key press, determine mode based on what is hit
 	if Input.is_action_just_pressed("selection"):
 		interacted_this_hold.clear()
-		firstobject = null
+		current_mode = ""
 
-	# While holding the key, update mode based on what you're looking at and select/deselect accordingly
-	if Input.is_action_pressed("selection"):
-		# Update current_mode based on raycast target at this moment
 		if selectray.is_colliding():
 			var hit = selectray.get_collider()
-
 			if hit.is_in_group("Selected"):
 				current_mode = "deselect"
 			elif hit.is_in_group("Selectable"):
 				current_mode = "select"
 			else:
-				current_mode = ""
+				current_mode = "select"
 		else:
-			current_mode = ""
+			current_mode = "select"  # <<<<< Add this else here!
 
-		# Perform selection/deselection if mode is set
-		if current_mode != "":
-			if selectray.is_colliding():
-				var hit = selectray.get_collider()
+	# While holding, apply mode to every new selectable hit by raycast
+	if Input.is_action_pressed("selection") and current_mode != "":
+		if selectray.is_colliding():
+			var hit = selectray.get_collider()
 
-				if hit.is_in_group("Selectable"):
-					if not interacted_this_hold.has(hit):
-						interacted_this_hold[hit] = true
+			if hit.is_in_group("Selectable") and not interacted_this_hold.has(hit):
+				interacted_this_hold[hit] = true
 
-						if current_mode == "select" and not hit.is_in_group("Selected"):
-							hit.add_to_group("Selected")
-							print("Selected: ", hit.name)
+				if current_mode == "select" and not hit.is_in_group("Selected"):
+					hit.add_to_group("Selected")
+					print("Selected: ", hit.name)
+				elif current_mode == "deselect" and hit.is_in_group("Selected"):
+					hit.remove_from_group("Selected")
+					print("Deselected: ", hit.name)
 
-						elif current_mode == "deselect" and hit.is_in_group("Selected"):
-							hit.remove_from_group("Selected")
-							print("Deselected: ", hit.name)
-
-	# Clear mode and interaction tracking when key is released
+	# Reset mode on key release
 	if Input.is_action_just_released("selection"):
 		current_mode = ""
 		interacted_this_hold.clear()
-		firstobject = null
+
+	# Press X to clear all selections
+	if Input.is_action_just_pressed("deselect"):
+		var selected_nodes = get_tree().get_nodes_in_group("Selected")
+		for node in selected_nodes:
+			node.remove_from_group("Selected")
