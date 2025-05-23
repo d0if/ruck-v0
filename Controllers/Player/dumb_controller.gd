dc1d9fee132b6aed1a28507d0484095d6bdc963b
doif-pc <josh.is.ahern@gmail.com>
2025-05-22 18:22:27 -0600

    05-22-25 sync

--- Controllers/Player/dumb_controller.gd
+++ Controllers/Player/dumb_controller.gd
@@ -1,1 +1,4 @@
-extends Node
+extends "res://Controllers/rigidbodycontroller.gd"
+
+func _process(delta: float) -> void:
+	DebugUtils.f3_main("min contact pitch test", self.min_contact_pitch)
--- Controllers/rigidbodycontroller.gd
+++ Controllers/rigidbodycontroller.gd
@@ -1,1 +1,19 @@
-extends Node
+extends RigidBody3D
+class_name RigidBodyController
+
+@export var MAX_ANGLE_WALK: float = 0.0
+var min_contact_pitch: float = 0.0
+var contact_norm_2d: Vector2 = Vector2(0.0,0.0)
+var contact_norm_3d: Vector3 = Vector3(0.0,0.0,0.0)
+
+#collect some info
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	if state.get_contact_count() > 0:
+		min_contact_pitch = 999.0
+		for contact_idx in range(0, state.get_contact_count()):
+			var norm = state.get_contact_local_normal(contact_idx)
+			var test_pitch = abs(Vector3(0.0, 1.0, 0.0).angle_to(norm))
+			if min_contact_pitch > test_pitch:
+				contact_norm_2d = Vector2(norm.x, norm.z).normalized()
+				contact_norm_3d = norm
+				min_contact_pitch = test_pitch
