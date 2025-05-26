37c6f3b97edea9df83344c5372fc7525028f57d2
doif-pc <josh.is.ahern@gmail.com>
2025-05-26 11:45:43 -0600

    05-26-25

--- Controllers/Player/dumb_controller.gd
+++ Controllers/Player/dumb_controller.gd
@@ -1,4 +1,9 @@
 extends "res://Controllers/rigidbodycontroller.gd"
 
+var move_angle: float = 0.0
+
 func _process(delta: float) -> void:
-	DebugUtils.f3_main("min contact pitch test", self.min_contact_pitch)
+	move_angle = MathUtils.cap_radians(move_angle + delta / 2.0)
+	#target_velocity = Vector3(1.0, 0.0, 0.0).rotated(Vector3(0.0,1.0,0.0),move_angle)
+	target_velocity = Vector3(0.0, 0.0, 0.0)
+	DebugUtils.f3_main("dumb controller move angle", move_angle)
--- Controllers/rigidbodycontroller.gd
+++ Controllers/rigidbodycontroller.gd
@@ -1,34 +1,18 @@
-extends RigidBody3D
+extends CharacterBody3D
 class_name RigidBodyController
-
-@export var MAX_ANGLE_WALK: float = 0.0
-@export var MAX_ANGLE_JUMP: float = 0.0
-@export var MIN_ANGLE_SLIDE: float = 0.0
-var min_contact_pitch: float = 0.0
-var min_contact_norm_2d: Vector2 = Vector2(0.0,0.0)
-var min_contact_norm_3d: Vector3 = Vector3(0.0,0.0,0.0)
-var max_contact_pitch: float = 0.0
-var max_contact_norm_2d: Vector2 = Vector2(0.0,0.0)
-var max_contact_norm_3d: Vector3 = Vector3(0.0,0.0,0.0)
 
 @export var ANGLE_VELOCITY_PUNISHMENT: Curve
 
-#collect some info
-func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	if state.get_contact_count() > 0:
-		min_contact_pitch = 999.0
-		max_contact_pitch = 0.0
-		for contact_idx in range(0, state.get_contact_count()):
-			var norm = state.get_contact_local_normal(contact_idx)
-			var test_pitch = abs(Vector3(0.0, 1.0, 0.0).angle_to(norm))
-			if min_contact_pitch > test_pitch:
-				min_contact_norm_2d = Vector2(norm.x, norm.z).normalized()
-				min_contact_norm_3d = norm
-				min_contact_pitch = test_pitch
-			if max_contact_pitch < test_pitch:
-				max_contact_norm_2d = Vector2(norm.x, norm.z).normalized()
-				max_contact_norm_3d = norm
-				max_contact_pitch = test_pitch
+var target_velocity: Vector3 = Vector3(0.0, 0.0, 0.0)
+
+var ignore_axis: Vector3 = Vector3(0.0, 1.0, 0.0) #set to 0,0,0 to ignore nothing
+
+
+func _ready() -> void:
+	pass
 
 func _physics_process(delta: float) -> void:
-	pass
+	target_velocity.y = -1.0
+	velocity = target_velocity
+	move_and_slide()
+	
